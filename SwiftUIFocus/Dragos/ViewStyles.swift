bc60d0087f9adca90ef56df614adffa61d5948d6
Dragos Fechete <dragos.fechete@icloud.com>
2025-04-16 02:22:36 +0300

    add view styles

---
+++ SwiftUIFocus/Dragos/ViewStyles.swift
@@ -0,0 +1,10 @@
+//
+//  ViewStyles.swift
+//  SwiftUIFocus
+//
+//  Created by Dragos on 16.04.2025.
+//
+
+// button styles
+// text field style cu validation
+// button, textfield, texteditor, stepper, toggle, list, datepicker
