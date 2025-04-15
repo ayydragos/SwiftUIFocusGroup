3a0e913cd4f7bca4aefd9fc905310c142f36905d
Dragos Fechete <Dragos.Fechete@garmin.com>
2025-04-15 15:32:11 +0300

    initial commit

---
+++ SwiftUIFocus/SwiftUIFocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFocus
+//
+//  Created by Fechete, Dragos on 15.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFocus/SwiftUIFocus/SwiftUIFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFocusApp.swift
+//  SwiftUIFocus
+//
+//  Created by Fechete, Dragos on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
